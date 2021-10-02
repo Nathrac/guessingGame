8433847bb56d074f13823a725825595196b3c1e7
Nathrac <nathanial.zaina@lethbridgecollege.ca>
2021-10-01 18:34:07 -0600

    Set up input response for player
    A color change response for when player selects a color

--- guessingGame/Assets/Scripts/PlayerInput.cs
+++ guessingGame/Assets/Scripts/PlayerInput.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    // Two tones of materials to have a sense of feedback for when player picks colors.
+    public Material drkMaterial;
+    public Material lightMaterial;
+
+    private Renderer render;
+
+
+    //Initialize render components.
+    private void Awake()
+    {
+        render = GetComponent<Renderer>();
+        render.enabled = true;
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +30,33 @@ public class PlayerInput : MonoBehaviour
     {
         
     }
+
+
+    //When player clicks a color, it swaps to a lighter version to confirm the color was picked
+    private void OnMouseDown()
+    {
+        PickedColor();
+    }
+
+    //Reverts colour back to noraml when not picked
+    private void OnMouseUp()
+    {
+        UnpickedColor();
+    }
+
+    //Renders colour to light material
+    public void PickedColor()
+    {
+        render.sharedMaterial = lightMaterial;
+    }
+
+    //Renders color to normal dark material
+    public void UnpickedColor()
+    {
+        render.sharedMaterial = drkMaterial;
+    }
+
+
+
 }
+
